fb32ddf8d2962571ae294652e2b8af3958445757
kavuthamapriya <priyarajendran018@gmail.com>
2024-11-28 11:01:56 +0530

    terraform files

--- superset-terraform/variables.tf
+++ superset-terraform/variables.tf
@@ -1,22 +1,22 @@
-# variable "ecs_cluster_name" {
-#   description = "The ECS Cluster Name"
-#   type        = string
-# }
+variable "ecs_cluster_name" {
+  description = "The ECS Cluster Name"
+  type        = string
+}
 
-# variable "task_arn" {
-#   description = "The ARN of the ECS task"
-#   type        = string
-# }
+variable "task_arn" {
+  description = "The ARN of the ECS task"
+  type        = string
+}
 
-# variable "subnet_id" {
-#   description = "The ID of the subnet"
-#   type        = string
-# }
+variable "subnet_id" {
+  description = "The ID of the subnet"
+  type        = string
+}
 
-# variable "vpc_id" {
-#   description = "The VPC ID"
-#   type        = string
-# }
+variable "vpc_id" {
+  description = "The VPC ID"
+  type        = string
+}
 
 variable "cluster_id" {
   description = "The ECS Cluster ID"
